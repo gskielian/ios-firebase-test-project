6aec62844227639325f01df00bd3b1510be80b20
gskielian <gregory.kielian@gmail.com>
2016-08-03 04:10:58 -0700

    add simple button with console log (no firebase in this step)

--- Firebase-Test-iOS/ViewController.m
+++ Firebase-Test-iOS/ViewController.m
@@ -17,6 +17,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [button setTitle:@"Press Me" forState:UIControlStateNormal];
+    [button sizeToFit];
+    
+    // Set a new (x,y) point for the button's center
+    button.center = CGPointMake(320/2, 60);
+    
+    
+    // Add an action in current code file (i.e. target)
+    [button addTarget:self action:@selector(buttonPressed:)
+     forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    [self.view addSubview:button];
+}
+
+- (void)buttonPressed:(UIButton *)button {
+    NSLog(@"Button Pressed");
 }
 
 - (void)didReceiveMemoryWarning {
